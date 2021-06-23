e48e8d559a7d0418b2e729d48fdb3a78a55fe861
5castles <80058037+5castles@users.noreply.github.com>
2021-06-24 00:33:01 +0900

    Update index.js

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -5,15 +5,15 @@ const hourHand = document.querySelector('.hour-hand'),
 function getTime(){
     const time = new Date();
     const seconds = time.getSeconds();
-    const secondsDegrees = ((seconds/60) * 3.6) + 90;
+    const secondsDegrees = ((seconds/60) * 360) + 90;
     secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
 
     const minutes = time.getMinutes();
-    const minutesDegrees = ((minutes/60) * 3.6) + 90;
+    const minutesDegrees = ((minutes/60) * 360) + 90;
     minHand.style.transform=`rotate(${minutesDegrees}deg)`;
 
     const hours = time.getHours();
-    const hoursDegrees = ((hours/12) * 3.6) + 90;
+    const hoursDegrees = ((hours/12) * 360) + 90;
     hourHand.style.transform=`rotate(${hoursDegrees}deg)`;
 
     console.log(hours, minutes, seconds)
@@ -23,3 +23,12 @@ function init(){
    
 }
 init();
+
+/*
+    CSS
+    transform: rotate(~deg);
+    transform-origin:      // transform 기준점 설정
+    transition: all 1s;
+
+
+*/
